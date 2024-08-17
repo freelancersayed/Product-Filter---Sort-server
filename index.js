0307792de2244d817b334219fd7ce437f36c586a
Md Sayed Hossain <freelancersayed45@gmail.com>
2024-08-17 17:26:35 +0600

    clear secret key and password

--- index.js
+++ index.js
@@ -8,11 +8,6 @@ app.use(cors());
 app.use(express.json());
 
 
-// findItfast
-// aOFZo2rRrrT6EjPg
-
-
-
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ehqhw1m.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
